fb6b986e6d009969f2f9db235c0719577d26360f
juant8560 <microfonos-broncear-0f@icloud.com>
2026-02-17 17:53:14 -0600

    Actualización de idioma, tamaña de letras del acronimo y movimiento del banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  image: {
+    domains: ['shzrtpndvthshvttizew.supabase.co'],
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@styles': path.resolve(__dirname, './src/styles'),
+        '@lib': path.resolve(__dirname, './src/lib'),
+        '@services': path.resolve(__dirname, './src/services'),
+      },
+    },
+  },
 });
